233eea0b1a248903c8b5af0cac9dbd993a95aa84
Patelriyaaa <91497158+Patelriyaaa@users.noreply.github.com>
2023-05-07 16:54:50 -0700

    Add files via upload
    changes in node.cpp and node.h and algorithm.cpp

---
+++ node.h
@@ -0,0 +1,12 @@
+﻿Public:
+
+
+//heuristic for A*
+enum class HeuristicType {
+  MisplacedTiles,
+  EuclideanDistance
+};
+
+
+//new constructor
+Node(const Puzzle& puzz, Node* par, int c, HeuristicType heuristic_type);
