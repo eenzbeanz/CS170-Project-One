0161e5db3799560ce2400086aea9f887b75d8578
mrini01 <91498111+mrini01@users.noreply.github.com>
2023-05-07 12:16:13 -0700

    Update Algorithm.h
    A* misplaced tile algo start stuff

--- Algorithm.h
+++ Algorithm.h
@@ -10,11 +10,18 @@ using namespace std;
 class Algorithm {
     private:
         Node* initial_node;
+        priority_queue<Node> open_list;
+        // a function that checks if node has been visited
+        bool visited(const Node& node, const vector<Node>& closed_list);
+        //mostly for output purposes
+        int expanded_nodes; 
+        int max_nodes_queue; 
+        int goal_depth; 
 
     public:
         Algorithm(const vector<int>& initial); 
         void uniform_cost_search();
-        void a_star_search();
+        void a_star_misplaced_tile();
         void a_star_search_euclid();
 };
 
